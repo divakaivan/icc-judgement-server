e679c6575a85aee92b0ce8c42a8b57c6572c28c8
divakaivan <54508530+divakaivan@users.noreply.github.com>
2020-03-05 20:40:48 +0000

    adding CORS middleware

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const Case = require("./models/Case");
 const User = require("./models/User");
-
+const cors = require("cors");
 // bring in graphql-express middleware
 const {graphiqlExpress, graphqlExpress} = require("apollo-server-express");
 const {makeExecutableSchema} = require("graphql-tools");
@@ -21,6 +21,14 @@ mongoose.connect(`mongodb+srv://divakaivan:${process.env.DB_PASS}@cluster0-cniio
     .catch(err=>console.log(err));
 
 const app = express();
+
+//handle CORS
+const corsOptions = {
+    origin: 'http://localhost:3000',
+    credentials: true // credentials is needed for apollo client to work correctly
+};
+
+app.use(cors(corsOptions));
 
 // create graphiql application
 app.use('/graphiql', graphiqlExpress({endpointURL: '/graphql'}));
