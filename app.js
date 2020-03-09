071bcb78ff26780a8ba8208a0f8b0e72b4719d2c
divakaivan <54508530+divakaivan@users.noreply.github.com>
2020-03-09 22:21:33 +0000

    adding middleware to get currentUser

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const bodyParser = require("body-parser");
 const Case = require("./models/Case");
 const User = require("./models/User");
 const cors = require("cors");
+const SECRET = require("./secret");
+const jwt = require("jsonwebtoken");
+
 // bring in graphql-express middleware
 const {graphiqlExpress, graphqlExpress} = require("apollo-server-express");
 const {makeExecutableSchema} = require("graphql-tools");
@@ -30,18 +33,34 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
+// set up JWT authentication middleware
+app.use(async (req, res, next) => {
+    const token = req.headers['authorization'];
+    if (token !== 'null') {
+        try {
+            const currentUser = await jwt.verify(token, SECRET);
+            req.currentUser = currentUser;
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    next();
+});
+
 // create graphiql application
 app.use('/graphiql', graphiqlExpress({endpointURL: '/graphql'}));
 
 
 // connect schemas with graphql
-app.use('/graphql', bodyParser.json(), graphqlExpress({
+app.use('/graphql', bodyParser.json(), graphqlExpress(({currentUser})=>({
     schema,
     context: {
         Case,
-        User
+        User,
+        currentUser
     }
-}));
+})));
 
 const PORT = 4444;
 
